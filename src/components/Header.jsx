1d900b04f3d1ba198bcebb5625759ca263a1014c
Sarah Schneider <mureb_rosa@hotmail.com>
2024-02-23 14:27:47 -0300

    start app and build header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header () {
+    return (
+        <header id="header">
+            <img src="investment-calculator-logo.png" alt="Investment Calculator App" />
+            <h1>Investment Calculator</h1>
+        </header>
+    );
+};
